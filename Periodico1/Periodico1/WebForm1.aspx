387462dd8e8109bbfb4ed717ebc3f09c371927bf
Juan Sebastian Vanegas <sebastian44van@gmail.com>
2017-07-05 20:49:35 -0500

    Se sube ejemplo de conexión a Oracle.

---
+++ Periodico1/Periodico1/WebForm1.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Periodico1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox runat="server" ID="txtTema" />
+        <asp:TextBox runat="server" ID="txtIcono" />
+        <asp:Button Text="Guadar Tema" runat="server" ID="btnGuardarTema" OnClick="btnGuardarTema_Click" />
+        <asp:Button Text="Crear Todo" runat="server" ID="btnCrearTodo" OnClick="btnCrearTodo_Click" />
+    </div>
+    <div>
+        <asp:Button Text="Consultar Temas" runat="server" ID="btnConsultarTemas" OnClick="btnConsultarTemas_Click" />
+        <asp:GridView runat="server" ID="gridTemas"></asp:GridView>
+    </div>
+        
+    </form>
+</body>
+</html>
---
+++ Periodico1/Periodico1/WebForm1.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Periodico1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnGuardarTema_Click(object sender, EventArgs e)
+        {
+            using (var cnn = new PeriodicoEntities())
+            {
+                cnn.TEMA.Add(new TEMA() { TEMA1=txtTema.Text, ICONO=txtIcono.Text });
+                cnn.SaveChanges();
+            }
+        }
+
+        protected void btnConsultarTemas_Click(object sender, EventArgs e)
+        {
+            using (var cnn = new PeriodicoEntities())
+            {
+                gridTemas.DataSource = cnn.TEMA.ToList();
+                gridTemas.DataBind();
+            }
+        }
+
+        protected void btnCrearTodo_Click(object sender, EventArgs e)
+        {
+            using (var cnn = new PeriodicoEntities())
+            {
+                var autor = new AUTOR() {
+                    NOMBRE = "Ricardo Jorge",
+                    TITULO = "El Corresponsal"                        
+                };
+                cnn.AUTOR.Add(autor);
+                cnn.SaveChanges();
+
+                var noticia = new NOTICIA() {
+                    TITULO = "Inicia Mundial Rusia 2018",
+                    CONTENIDO = "Texto de prueba",
+                    FOTO = "foto",
+                    FECHA = DateTime.Now,
+                    ID_TEMA = 1,
+                    ID_AUTOR = autor.ID_AUTOR
+                };
+                cnn.NOTICIA.Add(noticia);
+                cnn.SaveChanges();
+            }
+        }
+    }
+}
